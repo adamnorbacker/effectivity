d12f77a7b05e184359a0924ab6857a9222d0c790
klotim <klotim@users.noreply.github.com>
2018-08-23 13:48:31 +0200

    Update effectivity.js

--- effectivity.js
+++ effectivity.js
@@ -2,17 +2,28 @@
 var d = document;
 
 //getID function
-function getId(id) {
+function getId(id){
 	return d.getElementById(id);
 }
 
 //getClass function
-function getClass(c) {
+function getClass(c){
 	return d.getElementsByClassName(c);
 }
 
+function isClassOrId(s){
+	if(!isEmpty(getId(s))){
+		return getId(s);
+	} elseif(!isEmpty(getClass(s))){
+		return getClass(s);
+	} else {
+		console.error("No selector found");
+		return false;
+	}
+}
+
 //Check if variable is empty.
-function isEmpty (v) {
+function isEmpty (v){
 	var k;
 	if (v === "" || v === 0 || v === "0" || v === null || v === false || v === undefined ) {
 	 return true;
@@ -28,7 +39,7 @@ function isEmpty (v) {
    
    
  //Validate if email  
- function validateEmail(email) {
+ function validateEmail(email){
 	var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 	return re.test(String(email).toLowerCase());
 }
@@ -41,24 +52,42 @@ function getQueryString(qs){
 
 //Get current height of element
 function getHeight(s){
-	if(!isEmpty(getId(s))){
-	return getId(s).offsetHeight;
-	} elseif(!isEmpty(getClass(s))){
-	return getClass(s).offsetHeight;
+	if(isClassOrId(s) !== false){
+		isClassOrId(s).offsetHeight;
 	} else {
-	console.error("No selector found to calculate height");
-	return false;
+		console.error("No selector found to calculate height");
+		return false;
 	}
+	
 }
 
 //Get current width of element
 function getWidth(s){
-	if(!isEmpty(getId(s))){
-	return getId(s).offsetWidth;
-	} elseif(!isEmpty(getClass(s))){
-	return getClass(s).offsetWidth;
+	if(isClassOrId(s) !== false){
+		isClassOrId(s).offsetWidth;
 	} else {
-	console.error("No selector found to calculate width");
-	return false;
+		console.error("No selector found to calculate width");
+		return false;
 	}
+}
+
+//Hide element
+function hide(s){
+	if(isClassOrId(s) !== false){
+		isClassOrId(s).style.display = "none";
+	} else {
+		console.error("No selector found to hide");
+		return false;
+	}
+    
+}
+
+//Hide element
+function show(s){
+	if(isClassOrId(s) !== false){
+		isClassOrId(s).style.display = "block";
+	} else {
+		console.error("No selector found to show");
+		return false;
+	} 
 }
